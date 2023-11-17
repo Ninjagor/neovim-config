88f7f6c04f35b33abf81980006078809fd6a3611
Rohit Karthik <rohitismaster@gmail.com>
2023-11-17 17:11:46 -0500

    Fixed some color schemes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,9 @@ local M = {
   
   -- Colorschemes
   { "bluz71/vim-nightfly-guicolors" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
+  { 'rose-pine/neovim', name = 'rose-pine' },
 
   -- Comments
   { 
